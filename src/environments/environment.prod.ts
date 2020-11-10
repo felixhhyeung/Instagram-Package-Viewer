b757b70e686a3f953e79780f6e9951d115b869d9
Felix HH Yeung <felixhhyeung@hotmail.com>
2020-11-10 18:09:26 +0800

    release

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  packagesServer: "http://felixhhyeung.ddns.net:49151",
-  masterPassword: "xoqvweqsOXpqPAPishuqXctkUHqEYcWyLnuStWhUHBBFiN4$TrVTYYDoLgiNnSf9"
-};
